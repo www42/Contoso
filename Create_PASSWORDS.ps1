18782ecc8b66ff24f5b01d55d3e42f0d1eda705e
www42 <tjaekel0815@gmail.com>
2026-03-08 20:41:37 +0100

    Add doc

--- Create_PASSWORDS.ps1
+++ Create_PASSWORDS.ps1
@@ -1,5 +1,20 @@
-$clientSecret = Read-Host -Prompt 'Client Secret' -AsSecureString | ConvertFrom-SecureString
-$clientSecretPlainText = Read-Host -Prompt 'Client Secret'
+# This script creates a file "./PASSWORDS"
+#
+# "./PASSWORDS" is red by LOGIN.ps1 in order to log into Azure
+# "./PASSWORDS" is red by other script to get secret.
+#
+# 'Client Secret powershell' 
+#       ist das Client Secret für die App Registration 'powershell'
+#       Get-MgApplication -Filter "displayName eq 'powershell'"
+#
+# 'LocalAdmin password'
+#       ist das Password für einen lokalen Administrator (bei VMs z.B.)
+#
+# 'DomainAdmin password'
+#       ist das Password für den Domänen Administrator (Active Directory)
+
+$clientSecret = Read-Host -Prompt 'Client Secret powershell' -AsSecureString | ConvertFrom-SecureString
+$clientSecretPlainText = Read-Host -Prompt 'Client Secret powershell'
 $localAdminPassword = Read-Host -Prompt 'LocalAdmin password' -AsSecureString | ConvertFrom-SecureString
 $domainAdminPassword = Read-Host -Prompt 'DomainAdmin password' -AsSecureString | ConvertFrom-SecureString
 @{
