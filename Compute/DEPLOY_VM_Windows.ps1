026d94c8a2b33760bafeb82e2e4556f882eb0b23
www42 <tjaekel0815@gmail.com>
2026-07-23 16:02:29 +0200

    New structure

--- Compute/DEPLOY_VM_Windows.ps1
+++ Compute/DEPLOY_VM_Windows.ps1
@@ -1,20 +1,30 @@
-$location = 'westeurope'
-$rgName = 'rg-prod'
-$vnetName = 'vnet-prod'
-$vnet = Get-AzVirtualNetwork -Name $vnetName -ResourceGroupName $rgName
+$location          = 'swedencentral'
+$resourceGroupName = 'rg-infrastructure'
+$vmName            = 'vm-infrastructure-vm0'
+$vmComputerName    = 'VM0'
+$vmSize            = 'Standard_B2as_v2'     # defaults to 'Standard_D2as_v6'
+$vmAdminUserName   = 'LocalAdmin'
+$vnetName          = 'vnet-hub'
+$deploymentName    = 'Deploy-infrastructure-vm0'
+
+$passwordName = [string]::Concat($vmAdminUserName,'Password')
+$vmAdminPassword = Get-Content "./PASSWORDS" | ConvertFrom-Json | % { $_.$passwordName } | ConvertTo-SecureString
+$vnet = Get-AzVirtualNetwork -Name $vnetName
 $subnet = $vnet.Subnets | Where-Object Name -EQ 'Subnet0'
+$subnetId = $subnet.Id
 
 $params = @{
     location = $location
-    vmName = 'vm-prod-dc1'
-    vmComputerName = 'DC1'
-    vmAdminUserName = 'DomainAdmin'
-    vmAdminPassword = Get-Content "./PASSWORDS" | ConvertFrom-Json | % { $_.domainAdminPassword } | ConvertTo-SecureString
-    subnetId = $subnet.Id
+    vmName = $vmName
+    vmComputerName = $vmComputerName
+    vmSize = $vmSize
+    vmAdminUserName = $vmAdminUserName
+    vmAdminPassword = $vmAdminPassword
+    subnetId = $subnetId
 }
 
 New-AzResourceGroupDeployment `
-    -Name 'Deploy-prod-dc1' `
-    -ResourceGroupName $rgName `
+    -Name $deploymentName `
+    -ResourceGroupName $resourceGroupName `
     -TemplateFile '.\templates\virtualMachineWindows.bicep' `
     @params
