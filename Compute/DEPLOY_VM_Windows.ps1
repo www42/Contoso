58fe6451bd986841a3c0bad2645676dab77f64a2
www42 <tjaekel0815@gmail.com>
2026-07-30 06:43:41 +0200

    New vm param

--- Compute/DEPLOY_VM_Windows.ps1
+++ Compute/DEPLOY_VM_Windows.ps1
@@ -1,16 +1,18 @@
-$location          = 'swedencentral'
-$resourceGroupName = 'rg-infrastructure'
-$vmName            = 'vm-infrastructure-vm0'
-$vmComputerName    = 'VM0'
-$vmSize            = 'Standard_B2as_v2'     # defaults to 'Standard_D2as_v6'
+$location          = 'westeurope'
+$resourceGroupName = 'rg-hybrididentity'
+$vmName            = 'vm-hybrididentity-server1'
+$vmComputerName    = 'Server1'
+#$vmSize            = 'Standard_B2as_v2'     # defaults to 'Standard_D2as_v6'
 $vmAdminUserName   = 'LocalAdmin'
-$vnetName          = 'vnet-hub'
+$vnetName          = 'vnet-hybrididentity'
+$subnetName        = 'Subnet0'
 $deploymentName    = 'Deploy-infrastructure-vm0'
+$deploymentName    = "Deploy-$vmName"
 
 $passwordName = [string]::Concat($vmAdminUserName,'Password')
 $vmAdminPassword = Get-Content "./PASSWORDS" | ConvertFrom-Json | % { $_.$passwordName } | ConvertTo-SecureString
 $vnet = Get-AzVirtualNetwork -Name $vnetName
-$subnet = $vnet.Subnets | Where-Object Name -EQ 'Subnet0'
+$subnet = $vnet.Subnets | Where-Object Name -EQ $subnetName
 $subnetId = $subnet.Id
 
 $params = @{
